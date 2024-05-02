d4e4fd5c4bc0830e10f21dda1b5b1eee2f2cd048
mclacore <mclacore@gmail.com>
2024-05-02 11:18:56 -0700

    Finished leaf node, got help from solution

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,8 +1,19 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHtmlNode(unittest.TestCase):
   def test_props_to_html(self):
     node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
-    self.assertEqual(node.props_to_html(), "href=\"https://www.google.com\" target=\"_blank\"")
+    self.assertEqual(node.props_to_html(), " href=\"https://www.google.com\" target=\"_blank\"")
+
+
+class testLeafNode(unittest.TestCase):
+  def test_no_props(self):
+    node = LeafNode("p", "This is a paragraph of text.")
+    self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
+    
+  def test_with_props(self):
+    node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+    self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
