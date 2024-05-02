b3c8514b21e11d00b411aaae10100e3a52bc5dd2
mclacore <mclacore@gmail.com>
2024-05-02 16:03:08 -0700

    setting up some parent node stuff

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,3 +33,16 @@ class LeafNode(HTMLNode):
   
   def __repr__(self):
     return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+  def __init__(self, tag, children, props=None):
+    super().__init__(tag, None, children, props)
+
+  def to_html(self):
+    if self.tag is None:
+      raise ValueError("no tag found")
+    if self.children is None:
+      raise ValueError("no children found")
+    for child in self.children:
+      print(child)
