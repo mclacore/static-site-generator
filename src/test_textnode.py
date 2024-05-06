28827ef99bca200b4feec9761010831951a66954
mclacore <mclacore@gmail.com>
2024-05-06 14:51:44 -0700

    text node to html node, new split delimiter func

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,29 +1,42 @@
 import unittest
 
-from textnode import TextNode
+from textnode import (
+  TextNode,
+  text_type_text,
+  text_type_bold,
+  text_type_italic,
+  text_type_code,
+  text_type_link,
+  text_type_image
+)
 
 class TestTextNode(unittest.TestCase):
   def test_eq(self):
-    node = TextNode("This is a text node", "bold")
-    node2 = TextNode("This is a text node", "bold")
+    node = TextNode("This is a text node", text_type_text)
+    node2 = TextNode("This is a text node", text_type_text)
     self.assertEqual(node, node2)
 
 
   def test_url(self):
-    node = TextNode("This is a none url", "bold", None)
-    self.assertIsNone(node.url)
+    node = TextNode("This is a url", text_type_text, "https://google.com")
+    node2 = TextNode("This is a url", text_type_text, "https://google.com")
+    self.assertEqual(node, node2)
 
 
-  def test_not_eq(self):
-    node = TextNode("This is a text node", "bold")
-    node2 = TextNode("This is not a text node", "bold")
+  def test_not_eq_text(self):
+    node = TextNode("This is a text node", text_type_bold)
+    node2 = TextNode("This is not a text node", text_type_bold)
     self.assertNotEqual(node, node2)
 
 
-  def test_text_type(self):
-    node = TextNode("This is a text node", "bold")
-    node2 = TextNode("This is a text node", "italic")
+  def test_not_eq_type(self):
+    node = TextNode("This is a text node", text_type_bold)
+    node2 = TextNode("This is a text node", text_type_italic)
     self.assertNotEqual(node, node2)
+
+  def text_repr(self):
+    node = TextNode("This is a text node", text_type_text, "https://google..com")
+    self.assertEqual("TextNode(This is a text node, text, https://google.com)", repr(node))
 
 
 if __name__ == "__main__":
