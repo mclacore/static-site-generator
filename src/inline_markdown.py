de148f2d4881c9a7b6d5868eef54415d723977cc
mclacore <mclacore@gmail.com>
2024-05-14 13:30:06 -0700

    split nodes images/link

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,9 +1,13 @@
+import re
+
 from textnode import (
   TextNode,
   text_type_text,
   text_type_bold,
   text_type_italic,
   text_type_code,
+  text_type_image,
+  text_type_link
 )
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -24,5 +28,70 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
       else:
         split_nodes.append(TextNode(elements[i], text_type))
     new_nodes.extend(split_nodes)
+
+  return new_nodes
+
+
+def extract_markdown_images(text):
+  tuple = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+  return tuple
+
+
+def extract_markdown_links(text):
+  tuple = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+  return tuple
+
+
+def split_nodes_image(old_nodes):
+  new_nodes = []
+  for old_node in old_nodes:
+    if old_node.text_type != text_type_text:
+      new_nodes.append(old_node)
+      continue
+    original_text = old_node.text
+    image_tup = extract_markdown_images(original_text)
+    if len(image_tup) == 0:
+      new_nodes.append(old_node)
+      continue
+    for image in image_tup:
+      elements = original_text.split(f"![{image[0]}]({image[1]})", 1)
+      if len(elements) != 2:
+        raise ValueError("Invalid markdown, element not closed")
+      if elements[0] != "":
+        new_nodes.append(TextNode(elements[0], text_type_text))
+      new_nodes.append(
+        TextNode(
+          image[0],
+          text_type_image,
+          image[1]
+        )
+      )
+      original_text = elements[1]
+    if original_text != "":
+      new_nodes.append(TextNode(original_text, text_type_text))
+
+  return new_nodes
+
+def split_nodes_link(old_nodes):
+  new_nodes = []
+  for old_node in old_nodes:
+    if old_node.text_type != text_type_text:
+      new_nodes.append(old_node)
+      continue
+    original_text = old_node.text
+    link_tup = extract_markdown_links(original_text)
+    if len(link_tup) == 0:
+      new_nodes.append(old_node)
+      continue
+    for link in link_tup:
+      elements = original_text.split(f"[{link[0]}]({link[1]})", 1)
+      if len(elements) != 2:
+        raise ValueError("Invalid markdown, element not closed")
+      if elements[0] != "":
+        new_nodes.append(TextNode(elements[0], text_type_text))
+      new_nodes.append(TextNode(link[0], text_type_link, link[1]))
+      original_text = elements[1]
+    if original_text != "":
+      new_nodes.append(TextNode(original_text, text_type_text))
 
   return new_nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,10 @@
 import unittest
 
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import (
+  split_nodes_delimiter,
+  extract_markdown_images,
+  extract_markdown_links
+)
 
 from textnode import (
   TextNode,
@@ -22,6 +26,23 @@ class TestInlineMarkdown(unittest.TestCase):
       ],
       new_nodes
     )
+
+
+class TestExtractMarkdown(unittest.TestCase):
+  def test_extract_link(self):
+    text = "This is a text with a [link](https://google.com) and [another](https://boot.dev)"
+    extracted = [
+      ("link", "https://google.com"),
+      ("another", "https://boot.dev")
+    ]
+    self.assertListEqual(extract_markdown_links(text), extracted)
+    
+  def test_extract_image(self):
+    text = "This is a text with an ![image](https://imgur.com/foo/bar.png)"
+    extracted = [
+      ("image", "https://imgur.com/foo/bar.png")
+    ]
+    self.assertListEqual(extract_markdown_images(text), extracted)
     
   def test_bold_double(self):
     node = TextNode("This is a text with a **bolded** word and **another**", text_type_text)
