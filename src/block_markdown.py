8d3865e5bae133c75c5322f937937528d327aaeb
mclacore <mclacore@gmail.com>
2024-05-20 16:11:26 -0700

    Getting some md to blocks down

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,16 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown):
-  split_list = markdown.split("\n\n")
-  
+  result = []
+  blocks = markdown.split("\n\n")
+  for block in blocks:
+    if block == "":
+      continue
+    block = block.strip()
+    result.append(block)
+  return result
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,12 +1,56 @@
 import unittest
 
 from block_markdown import (
-  markdown_to_blocks
+  markdown_to_blocks,
+  block_type_paragraph,
+  block_type_code,
+  block_type_heading,
+  block_type_ordered_list,
+  block_type_quote,
+  block_type_unordered_list,
 )
 
 class TestBlockMarkdown(unittest.TestCase):
   def test_markdown_to_blocks(self):
-    string = markdown_to_blocks("")
-    block = ""
-    self.assertEqual(string, block)
+    markdown = """
+This is a **bolded** paragraph
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+    expected_blocks = [
+      "This is a **bolded** paragraph",
+      "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+      "* This is a list\n* with items"
+    ]
     
+    result_blocks = markdown_to_blocks(markdown)
+    self.assertEqual(result_blocks, expected_blocks)
+
+
+  def test_markdown_to_blocks_morelines(self):
+    markdown = """
+This is a **bolded** paragraph
+
+
+
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+    expected_blocks = [
+      "This is a **bolded** paragraph",
+      "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+      "* This is a list\n* with items"
+    ]
+    result_blocks = markdown_to_blocks(markdown)
+    self.assertEqual(result_blocks, expected_blocks)
+
+if __name__ == '__main__':
+  unittest.main()
